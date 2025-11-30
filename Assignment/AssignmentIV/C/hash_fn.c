fbf6c48bdf83c47e2d63e04102f54b013f769bef
周品岑 <245joymelissa@gmail.com>
2025-11-30 22:57:44 +0800

    Update developer information and hash functions

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -10,18 +10,23 @@
     - 2025/11/11: Initial implementation
     - 2025/11/17: Refactored to use hash_fn.h
 
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: PIN-CEN JOU <s1131527@mail.yzu.edu.tw>
  */
 
 #include "hash_fn.h"
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+    if (key < 0) {
+        key = -key; 
+    }
     return key % m;  // division method example
 }
 
 int myHashString(const char* str, int m) {
     unsigned long hash = 0;
-    // TODO: replace with your own design
+    int p = 31; 
+    for (int i = 0; str[i] != '\0'; i++) {
+        hash = (hash * p + (unsigned char)str[i]) % m;
+    }
     return (int)(hash % m); // basic division method
 }
