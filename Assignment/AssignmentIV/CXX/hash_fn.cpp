b41285ed551b6d97bdfc1f2aed6083cd18df6b12
周品岑 <245joymelissa@gmail.com>
2025-11-30 23:06:49 +0800

    Update hash_fn.cpp

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -10,17 +10,22 @@
     - 2025/11/11: Initial implementation
     - 2025/11/17: Refactored to use hash_fn.hpp
 
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: PIN-CEN JOU <s1131527@mail.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+    if (key < 0) {
+        key = -key;
+    }
     return key % m;  // basic division method
 }
 
 int myHashString(const std::string& str, int m) {
     unsigned long hash = 0;
-    // TODO: replace with your own design
+    const unsigned int p = 31;
+    for (char c : str) {
+        hash = hash * p + static_cast<unsigned char>(c);
+    }
     return static_cast<int>(hash % m);  // basic division method
 }
